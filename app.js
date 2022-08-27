ffd771f41d763f7b8eb295a6bf5cb8124a36fc8c
Sojo C Johny <sojocjohny37@gmail.com>
2022-08-27 10:38:23 +0530

    Made minor changes and rearranged code

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const pianoKeys = document.querySelectorAll('.key');
 function playSound(soundUrl){
-    new Audio(soundUrl).play()
+    const sound = new Audio(soundUrl)
+    sound.currentTime = 0
+    sound.play()
 }
 pianoKeys.forEach((pianoKey, i)=> {
     const num = i<9 ? '0'+(i+1) : (i+1);
     const soundUrl = 'sounds/key'+num+'.ogg';
-    pianoKey.addEventListener('mousedown', () => playSound(soundUrl))
+    pianoKey.addEventListener('mousedown', ()=>playSound(soundUrl))
 })
